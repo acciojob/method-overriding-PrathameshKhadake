3308e7f32c22fc573ea0b29c831b22e1b817a516
Prathamesh Khadake <pratham14785@gmail.com>
2023-08-10 16:45:29 +0530

    attempt-1

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,11 @@
+package com.driver;
+
+public class B extends A {
+    public B() {
+
+    }
+
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,7 @@
 package com.driver;
 
 public class Main {
-  
+    B b = new B();
+    String ans = b.meth();
+    String ans2 = b.meth();
 }
