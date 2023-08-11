0b9ef4d6abe7ea47e10e60534c8cd6ffd8807171
Prathamesh Khadake <pratham14785@gmail.com>
2023-08-11 22:51:26 +0530

    Attempt-1

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -2,10 +2,7 @@ package com.driver;
 
 public class B extends A {
 
-    B(){
-        super();
-    }
-
+    @Override
     public String meth(){
         return "Method is overridden in Extendend class B";
     }
